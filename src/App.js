220ecefb5477b6d878aa0fc40a195808ce3e1d37
Ayush Singhal <singhal.ayush27@gmail.com>
2023-06-07 04:41:12 +0530

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,22 +1,43 @@
-import logo from './logo.svg';
+import React, { useState } from 'react';
+import axios from 'axios';
 import './App.css';
 
 function App() {
+  const [url, setUrl] = useState('');
+  const [shortUrl, setShortUrl] = useState('');
+
+  const shortenUrl = async () => {
+    try {
+      const response = await axios.post('http://localhost:8080/shorten', { url });
+  
+      if (response.status === 200) {
+        setShortUrl(response.data);
+      } else {
+        setShortUrl('Error: ' + response.statusText);
+      }
+    } catch (error) {
+      setShortUrl('Error: ' + error.message);
+    }
+  };  
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>Link Shortener</h1>
+        <div className="search-container">
+          <input
+            type="text"
+            placeholder="Enter a URL"
+            value={url}
+            onChange={(e) => setUrl(e.target.value)}
+          />
+          <button onClick={shortenUrl}>Shorten</button>
+        </div>
+        {shortUrl && (
+          <div className="result">
+            <p>Short URL: {shortUrl}</p>
+          </div>
+        )}
       </header>
     </div>
   );
